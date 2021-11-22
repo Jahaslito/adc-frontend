0fe59a947c6569f82a50b032d5e66e036c628955
tmogoa <mogoa.tonny@gmail.com>
2021-11-22 21:28:30 +0300

    Configured fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        fontFamily: {
+            sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            serif: [...defaultTheme.fontFamily.serif],
+            mono: [...defaultTheme.fontFamily.mono],
+            heading: ["Open\\ Sans"],
+        },
     },
     variants: {
         extend: {},
