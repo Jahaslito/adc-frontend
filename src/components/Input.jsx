8317ab294cd0498c563feb1fdba6517c002eeb47
tmogoa <mogoa.tonny@gmail.com>
2021-12-02 15:17:55 +0300

    tweaks

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,7 +6,6 @@ import React from "react";
  * validate: pass error message for the field
  * all other will be passed input field:
  *  - type
- *  - placeholder(we don't need one)
  *  - value: the text value typed by the user
  *  - onChange: setter function to set value as user types
  */
@@ -17,7 +16,7 @@ const Input = ({ label, required, validate, ...props }) => {
                 {label} {required && <span className="text-red-500">*</span>}
             </span>
             <input
-                className="mt-2 border-b-2 border-primary px-2 py-1 focus:outline-none focus:border-blue-500 text-gray-600"
+                className="mt-2 border-b-2 border-gray-500 px-2 py-1 focus:outline-none focus:border-blue-500 text-gray-600"
                 {...props}
             ></input>
             <span className="text-xs text-red-500 mt-1">{validate}</span>
